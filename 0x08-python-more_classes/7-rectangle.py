5a2450a2493aae004ca7a414393f936b9e1c59fb
“aniekanmark55” <aniekanmark55@gmail.com>
2023-01-30 16:47:22 -0800

    python task

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -69,3 +69,15 @@ class Rectangle:
                     rectangle += str(self.print_symbol)
                 except Exception:
                     rectangle += type(self).print_symbol
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
+
+    def __repr__(self):
+        """returns a string representation of the rectangle"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """prints a message for every object that is deleted"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
